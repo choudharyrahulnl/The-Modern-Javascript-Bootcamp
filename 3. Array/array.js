b449e1121d177b37e8b5c80ec9e3a4de77c8f708
choudhary2205rahul <rahul.choudhary@swisscom.com>
2022-10-27 04:46:18 +0200

    initial commit

--- 3. Array/array.js
+++ 3. Array/array.js
@@ -33,6 +33,8 @@ console.log(arr4.includes(4)); // return false if not present
 console.log(arr4.indexOf(1)); // return index of element
 console.log(arr4.indexOf(2));
 
+
+
 console.log('***************** Reverse ***********************');
 let arr5 = [1,2,3];
 console.log(arr5.reverse()); // changes original array
@@ -60,3 +62,34 @@ console.log(arr7) // at index 1 don't delete anything and add 10
 arr7 = [1,2,3,4,5,6];
 console.log(arr7.splice(1,0,10,20)); // return deleted elements
 console.log(arr7) // at index 1 don't delete anything and add 10
+
+console.log('***************** Sort ***********************');
+let arr8 = ['z','m','d','b','a'];
+console.log(arr8.sort());
+
+arr8 = [5,10000,4,3,2,1]; // it converts num into char code and then sort
+console.log(arr8.sort());
+
+console.log('***************** Sort ***********************');
+let arr9 = [1,2,3,4];
+let arr10 = arr9;
+arr9.push(5);
+console.log(arr10); // array are reference type
+
+let fruit = 'orange';
+let newFruit = fruit;
+fruit = 'grapes';
+console.log(newFruit); // primitive are value type
+
+console.log('***************** Const with Array ***********************');
+const arr11 = [1,2,3,4];
+arr11.push(5); // Here const refer to array location and not the array
+console.log(arr11); // that's why we can change the array, but we can't change array address is const
+// arr11 = [1,2,3,4]; // not allowed with const
+
+console.log('***************** Nested Array ***********************');
+let arr12 = [ [1,2], [3,4] ];
+console.log(arr12);
+console.log(arr12[0]); // gives first nested array
+console.log(arr12[0][0]); // gives first nested array, first element
+console.log(arr12[0][1]); // gives first nested array, second element
